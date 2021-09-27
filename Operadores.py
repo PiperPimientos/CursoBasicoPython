643b24d5b914de8fdf0e7aa9a93055480bba0de3
felipe <feliperestrepomeneses@gmail.com>
2021-09-27 16:41:25 -0500

    Agregamos el archivo Operadores.py, para empezar a comprender como funcionan los operadores numericos o aritmeticos

---
+++ Operadores.py
@@ -0,0 +1,31 @@
+#Veremos cuales son los operadores aritmeticos
+#Suma
+5 + 5
+#En consola, press enter y de una sale el resultado
+#Desde el editor de codigo hay que hacer un print
+print(5 + 5)
+#Podemos restar
+5 - 5
+#en la consola sale directo 0
+#multiplicar
+5 * 2
+#dividir
+10 / 2
+#Division entera
+10 // 2
+#sacar el modulo, que es el numero que nos permite ver el resto de la division
+10 % 2
+#elevar al exponente
+2 ** 2
+
+
+
+
+
+#Tenemos que saber que Python tiene la regla de precedencia de operadores: si ponemos operación como  5 + 5 * 2, lo que se tendría que resolver primero es la multiplicación. Se resuelven primero los paréntesis, luego las potencias y raíces, multiplicaciones y divisiones y ultimo sumas y restas.
+#PEMDAS: Parentesis, exponentes, multiplicación, división, adicción y substración
+#Para hacer las raíces cuadradas, se pueden usar los exponentes ** divido la mitad (1/2) o 0.5.
+
+
+
+
