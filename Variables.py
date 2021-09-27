6d21850965009656f7dfe317417edd906a304daf
felipe <feliperestrepomeneses@gmail.com>
2021-09-27 17:33:30 -0500

    agregamos el archivo Variables.py para aprender sobre las variables en python, que desde una definicion basica propia, son valores asignados a identificadores

---
+++ Variables.py
@@ -0,0 +1,23 @@
+#Veremos las Variables
+#Son valores asignados a un identificador, primero el identificador, luego la asignacion y finalmente el valor
+numero = 3
+#Para imprimir en pantalla utilizamos print mas el identificador, en parentesis
+print(numero)
+#Si ejecutamos en consola, sale 3
+
+#Tenemos un par de numeros adicionales
+numero1 = 5
+numero2 = 6
+#Podemos sumarlos con los identificadores
+numero1 + numero2
+#El resultado es directamente 11
+
+
+#Podemos asignar un valor nuevo a una variable que ya tenia valor
+numero1 = 3
+
+
+
+#Incluso podremos asignar el resultado de la operacion entre dos variables, dentro de una variable
+numero_resultado = numero1 + numero2
+
