2317a53f3915d7a70cebd1050f72d1fe6d7fff4a
felipe <feliperestrepomeneses@gmail.com>
2021-09-28 07:20:51 -0500

    agregamos el archivo TiposDatos.py para aprender a realizar sobre las variables inputs y cambios de tipos de datos con las funciones str, int, float

---
+++ TiposDatos.py
@@ -0,0 +1,15 @@
+#Input
+numero1 = input("Escribe un numero: ") #Supose the number is '3'
+#Asi ya la variable numero1 queda con el valor que le demos en la consola
+#Sumando dos numeros que estan tipo string
+numero1 + '5'
+#Si ejecuto, veremos que el valor sera 35, se concatenaron lo dos strings
+
+#Puedo convertir un valor de tipo string a otro tipo, como int
+numero1 = int(numero1)
+#Tambien podemos convertir por ejemplo float a int
+numero_decimal = 4.5
+numero_decimal = int(numero_decimal)
+#O directamente en la consola
+int(4.5)
+#Ambos dejan como resultado un numero int, 4
