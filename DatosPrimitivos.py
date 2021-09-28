52cb0d9d4dc4c6bd28989b86f2e209c41c88b36a
felipe <feliperestrepomeneses@gmail.com>
2021-09-27 23:21:17 -0500

    Agregamos el archivo DatosPrimitivos.py para agregar que a los tipo de datos que ya habiamos visto, esto es, los int y los float, existen tambien los string y boolean

---
+++ DatosPrimitivos.py
@@ -0,0 +1,13 @@
+#String
+nombre = "Esto es un String"
+
+#Concatenacion de variables
+nombre1 = "Felipe"
+nombre2 = "Restrepo"
+nombre_completo = nombre1 + nombre2 #Imprime Felipe Restrepo
+nombre_multiplicado = nombre1 * 2 #Imprime FelipeFelipe
+nombre_concatenado = nombre1 + ", " + nombre2 #Imprime Felipe, Restrepo
+
+
+#Booleanos
+es_estudiante = True #Si ejecutamos, sale True
