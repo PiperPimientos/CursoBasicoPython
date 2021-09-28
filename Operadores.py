3ca696735a4340d3d869695486c9b109df9510b2
felipe <feliperestrepomeneses@gmail.com>
2021-09-28 08:00:08 -0500

    Modificamos el archivo Operadores.py, para aprender sobre operadores logicos y de comparacion

--- Operadores.py
+++ Operadores.py
@@ -29,3 +29,52 @@ print(5 + 5)
 
 
 
+
+#Ahora veremos como tal los operadores logicos y de comparacion
+#Imaginemos que tenemos un estudiante que no trabaja, como lo expreso?
+es_estudiante = True
+trabaja = False
+
+#Si nos planteamos: esta persona es estudiante y trabaja. Eso es un False, como lo comprobamos?
+es_estudiante and trabaja
+#la ejecucion es False
+
+
+
+
+#AHORA, si decimos que esta persona es estudiante o trabaja Utilizaremos el operador or
+es_estudiante or trabaja
+
+
+
+
+#AHORA, tenemos el operador not que nos servirá para invertir el valor booleano
+#Si yo digo not es_estudiante y el valor original de es_estudiante era True, cambiara a False
+not es_estudiante
+#Hasta aqui los operadores logicos
+
+
+
+
+
+
+#Operadores de comparacion
+
+
+#Primero veremos el operador de igualdad ==
+#Como podemos saber que lo que hay dentro de las variables es igual? Con el operador de ==
+numero1 = 5
+numero2 = 5
+numero1 == numero2
+
+#El opuesto a el operador de igualdad es el operador, distinto !=
+numero3 = 3
+numero1 != numero3 #Imprime True
+
+#Tambien estan los operadores de mayor o menor que
+numero3 > numero1 #Imprime False
+
+
+#Tambien estan los operadores de mayor igual o menor igual que >= <=
+numero1 >= numero2 #Imprime True, porque ambos valores son iguales
+
